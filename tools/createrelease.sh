dc0a2c023aef78fb88e43c0c5a1d77c3659c99bb
Carsten Munk <carsten@maemo.org>
2011-10-14 15:36:08 +0200

    Add armv6l as part of releases

--- tools/createrelease.sh
+++ tools/createrelease.sh
@@ -13,11 +13,13 @@ fi
 $TOOLS/dumpbuild "$API" "Core:i586" Core:i586:$RELEASE Core_i586 "i586"
 $TOOLS/dumpbuild "$API" "Core:armv7l" Core:armv7l:$RELEASE Core_armv7l "i586 armv7el"
 $TOOLS/dumpbuild "$API" "Core:armv7hl" Core:armv7hl:$RELEASE Core_armv7hl "i586 armv8el"
+$TOOLS/dumpbuild "$API" "Core:armv6l" Core:armv6l:$RELEASE Core_armv6l "i586 armv7el"
 
-rm -f obs-repos/Core:i586:latest obs-repos/Core:armv7l:latest obs-repos/Core:armv7hl:latest
+rm -f obs-repos/Core:i586:latest obs-repos/Core:armv7l:latest obs-repos/Core:armv7hl:latest obs-repos/Core:armv6l:latest
 ln -s Core:i586:$RELEASE obs-repos/Core:i586:latest
 ln -s Core:armv7l:$RELEASE obs-repos/Core:armv7l:latest
 ln -s Core:armv7hl:$RELEASE obs-repos/Core:armv7hl:latest
+ln -s Core:armv6l:$RELEASE obs-repos/Core:armv6l:latest
 
 
 grab_build()
@@ -43,6 +45,7 @@ grab_build()
 grab_build Core:/i586/Core_i586 i586 
 grab_build Core:/armv7l/Core_armv7l armv7l
 grab_build Core:/armv7hl/Core_armv7hl armv7hl
+grab_build Core:/armv7hl/Core_armv6l armv6l
 
 rsync -aHx --progress obs-repos/Core\:*\:$RELEASE obs-repos/Core\:*\:latest merreleases@monster.tspre.org:~/public_html/obs-repos/
 rsync -aHx --progress releases/$RELEASE merreleases@monster.tspre.org:~/public_html/releases/
